bbb466a3d7f05768d58047aa42733ff3108e8f8d
Din <dinmukhamed.mailibay@gmail.com>
2024-07-27 02:01:03 -0700

    disable python build

--- baml-lib/baml/build.rs
+++ baml-lib/baml/build.rs
@@ -1,3 +1,4 @@
 fn main() {
-    pyo3_build_config::add_extension_module_link_args();
+    // Uncomment this line if you need to build for Python
+    // pyo3_build_config::add_extension_module_link_args();
 }
